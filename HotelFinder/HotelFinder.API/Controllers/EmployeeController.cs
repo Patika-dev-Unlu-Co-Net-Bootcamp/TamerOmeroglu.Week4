4e8d83f0cdf43b127536505a1aedde84314c1691
Tamer Omeroglu <tameromeroglu@trex.com.tr>
2022-02-07 00:53:58 +0300

    Week4 Tasks Completed

---
+++ HotelFinder/HotelFinder.API/Controllers/EmployeeController.cs
@@ -0,0 +1,123 @@
+﻿using HotelFinder.API.Exceptions;
+using HotelFinder.DataAccess;
+using HotelFinder.Entity;
+using HotelFinder.Entity.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly HotelFinderDbContext _context;
+
+        public EmployeeController(HotelFinderDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployee()
+        {
+            var employees = _context.Employees.Select(x => ItemToDTO(x)).ToList();
+
+            if (employees.Count == 0)
+            {
+                throw new NotFoundException("No registered employee");
+            }
+            return Ok(employees);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee == null)
+            {
+                throw new NotFoundException("Employee not found");
+            }
+            return Ok(ItemToDTO(employee));
+        }
+
+
+        [HttpPost("create")]
+        public IActionResult CreateEmployee([FromBody] EmployeeCreateDto employeeDto)
+        {
+
+            var hotelExits = HotelIdExists(employeeDto.HotelId);
+
+            if (!hotelExits)
+            {
+                throw new BadRequestException("There is no hotel with the id in your request");
+            }
+
+            var employee = new Employee()
+            {
+                Name = employeeDto.Name,
+                Surname = employeeDto.Surname,
+                HotelId = employeeDto.HotelId
+            };
+
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+
+
+            return CreatedAtAction(
+                    nameof(GetEmployee),
+                    new { id = employee.Id },
+                    ItemToDTO(employee));
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, [FromBody] EmployeeCreateDto employeeDto)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee == null)
+            {
+                throw new NotFoundException("Employee Not Found");
+            }
+
+            employee.Name = employeeDto.Name;
+            employee.Surname = employeeDto.Surname;
+            employee.HotelId = employeeDto.HotelId;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee = _context.Employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee == null)
+            {
+                throw new NotFoundException("Employee Not Found");
+            }
+
+            _context.Remove(employee);
+
+            return Ok();
+        }
+
+        private bool HotelIdExists(int id) => _context.Hotels.Any(e => e.Id == id);
+
+        private static EmployeeDto ItemToDTO(Employee todoItem) =>
+        new EmployeeDto
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            Surname = todoItem.Surname,
+            HotelId = todoItem.HotelId
+        };
+
+    }
+}
---
+++ HotelFinder/HotelFinder.API/Controllers/HotelController.cs
@@ -0,0 +1,111 @@
+﻿using HotelFinder.API.Exceptions;
+using HotelFinder.DataAccess;
+using HotelFinder.Entity;
+using HotelFinder.Entity.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace HotelFinder.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly HotelFinderDbContext _context;
+        public HotelController(HotelFinderDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllHotel()
+        {
+            var hotels = _context.Hotels.Select(x => ItemToDTO(x)).ToList();
+
+            if (hotels.Count == 0)
+            {
+                throw new NotFoundException("No registered hotel");
+            }
+            return Ok(hotels);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetHotel(int id)
+        {
+            var hotel = _context.Hotels.FirstOrDefault(x => x.Id == id);
+
+            if (hotel == null)
+            {
+                throw new NotFoundException("Hotel not found");
+            }
+            return Ok(ItemToDTO(hotel));
+        }
+
+
+        [HttpPost("create")]
+        public IActionResult CreateHotel([FromBody] HotelCreateDto hotelDto)
+        {
+            var hotel = new Hotel()
+            {
+                Name = hotelDto.Name,
+                City = hotelDto.City,
+                PostCode = hotelDto.PostCode
+            };
+
+            _context.Hotels.Add(hotel);
+            _context.SaveChanges();
+
+           
+            return CreatedAtAction(
+                    nameof(GetHotel),
+                    new { id = hotel.Id },
+                    ItemToDTO(hotel));
+        }
+
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateHotel(int id,[FromBody] HotelCreateDto hotelDto)
+        {
+            var hotel = _context.Hotels.FirstOrDefault(x => x.Id == id);
+
+            if (hotel == null)
+            {
+                throw new NotFoundException("Hotel Not Found");
+            }
+
+            hotel.Name = hotelDto.Name;
+            hotel.City = hotelDto.City;
+            hotel.PostCode = hotelDto.PostCode;
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteHotel(int id)
+        {
+            var hotel = _context.Hotels.FirstOrDefault(x => x.Id == id);
+
+            if (hotel == null)
+            {
+                throw new NotFoundException("Hotel Not Found");
+            }
+
+            _context.Remove(hotel);
+
+            return Ok();
+        }
+
+        private static HotelDto ItemToDTO(Hotel todoItem) =>
+        new HotelDto
+        {
+            Id = todoItem.Id,
+            Name = todoItem.Name,
+            City = todoItem.City,
+            PostCode = todoItem.PostCode
+        };
+    }
+}
